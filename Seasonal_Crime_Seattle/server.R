a1682a548d07c3304a8c203b3e934a4912ab9d4e
unknown <WALZEJON@uw.edu>
2019-06-03 18:43:18 -0700

    lubridate

--- Seasonal_Crime_Seattle/server.R
+++ Seasonal_Crime_Seattle/server.R
@@ -2,6 +2,7 @@ library(shiny)
 library(data.table)
 library(dplyr)
 library(ggplot2)
+library(lubridate)
 
 crime_data <- fread('~/../../Documents/seattle-seasonal-crime/data/Seattle_Crime_Stats_by_Police_Precinct_2008-Present.csv', header = TRUE,  stringsAsFactors = FALSE)
 crime_data$REPORT_DATE <- as.Date(crime_data$REPORT_DATE, "%m/%d/%Y")
@@ -10,7 +11,7 @@ crime_data$REPORT_DATE <- as.Date(crime_data$REPORT_DATE, "$Y-%m-%d")
 shinyServer(function(input, output) {
   
   output$crimePlot <- renderPlot({
-    
+    crime_data$REPORT_DATE <- format(temp, format="%m-%d")
     
     
   })
