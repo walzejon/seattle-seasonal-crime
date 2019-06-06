b00e5f1f95695186a702ee802037ccc77adac948
Danyalm <danyal.malik@gmail.com>
2019-06-05 17:15:38 -0700

    Plot data sorting fixed

--- Seasonal_Crime_Seattle/server.R
+++ Seasonal_Crime_Seattle/server.R
@@ -39,7 +39,7 @@ shinyServer(function(input, output) {
       crime_data <- subset(crime_data, REPORT_DATE >= "2019-09-01" & REPORT_DATE < "2019-12-01")
       
     }
-    else {
+    if(input$changeSeason == "Summer") {
       
       crime_data <- subset(crime_data, REPORT_DATE >= "2019-06-01" & REPORT_DATE < "2019-09-01")
       
@@ -104,7 +104,7 @@ shinyServer(function(input, output) {
       
       crime_data <- subset(crime_data, REPORT_DATE >= "2019-09-01" & REPORT_DATE < "2019-12-01")
     }
-    else {
+    if(input$changeSeason == "Summer") {
       
       crime_data <- subset(crime_data, REPORT_DATE >= "2019-06-01" & REPORT_DATE < "2019-09-01")
       
